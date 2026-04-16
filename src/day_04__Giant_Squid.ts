ff32415c810828d52316d7ae53843e131934e799
ea234 <anyang@gmx.de>
2026-04-16 17:07:01 +0200

    Day 04 - Part 2

--- src/day_04__Giant_Squid.ts
+++ src/day_04__Giant_Squid.ts
@@ -6,59 +6,145 @@ import * as readline from 'readline';
  * 
  * https://www.reddit.com/r/adventofcode/comments/r8i1lq/2021_day_4_solutions/
  * 
+ * 
  * /home/ea234/.nvm/versions/node/v20.16.0/bin/node ./dist/day04/day_04__Giant_Squid.js
  * 
  * Day 04 - Giant Squid
  * 
- * 22,13,17,11,0,
- * 
  *  Bingo Card 0
- *  22  13  17  11   0
- *   8   2  23   4  24
- *  21   9  14  16   7
- *   6  10   3  18   5
- *   1  12  20  15  19
+ *  22  13  17  11   0     22  13  17  11   0
+ *   8   2  23   4  24      8   2  23   4  24
+ *  21   9  14  16   7     21   9  14  16   7
+ *   6  10   3  18   5      6  10   3  18   5
+ *   1  12  20  15  19      1  12  20  15  19
  * 
  *  Bingo Card 1
- *   3  15   0   2  22
- *   9  18  13  17   5
- *  19   8   7  25  23
- *  20  11  10  24   4
- *  14  21  16  12   6
+ *   3  15   0   2  22      3  15   0   2  22
+ *   9  18  13  17   5      9  18  13  17   5
+ *  19   8   7  25  23     19   8   7  25  23
+ *  20  11  10  24   4     20  11  10  24   4
+ *  14  21  16  12   6     14  21  16  12   6
  * 
  *  Bingo Card 2
- *  14  21  17  24   4
- *  10  16  15   9  19
- *  18   8  23  26  20
- *  22  11  13   6   5
- *   2   0  12   3   7
+ *  14  21  17  24   4     14  21  17  24   4
+ *  10  16  15   9  19     10  16  15   9  19
+ *  18   8  23  26  20     18   8  23  26  20
+ *  22  11  13   6   5     22  11  13   6   5
+ *   2   0  12   3   7      2   0  12   3   7
  * 
+ * 7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+ * Nr.   0 =   7
+ * Nr.   1 =   4
+ * Nr.   2 =   9
+ * Nr.   3 =   5
+ * Nr.   4 =  11
+ * Nr.   5 =  17
+ * Nr.   6 =  23
+ * Nr.   7 =   2
+ * Nr.   8 =   0
+ * Nr.   9 =  14
+ * Nr.  10 =  21
+ * Nr.  11 =  24
+ * Nr.  12 =  10
+ * Nr.  13 =  16
+ * Nr.  14 =  13
+ * Nr.  15 =   6
+ * Nr.  16 =  15
+ * Nr.  17 =  25
+ * Nr.  18 =  12
+ * Nr.  19 =  22
+ * Nr.  20 =  18
+ * Nr.  21 =  20
+ * Nr.  22 =   8
+ * Nr.  23 =  19
+ * Nr.  24 =   3
+ * Nr.  25 =  26
+ * Nr.  26 =   1
+ * 
+ * -------------------------------------------------------------------------------------
  * 
  *  Bingo Card 0
- *  -1  -1  -1  -1  -1
- *   8   2  23   4  24
- *  21   9  14  16   7
- *   6  10   3  18   5
- *   1  12  20  15  19
+ *  22  13  17  11   0     22  13  -1  -1  -1
+ *   8   2  23   4  24      8  -1  -1  -1  -1
+ *  21   9  14  16   7     -1  -1  -1  -1  -1
+ *   6  10   3  18   5      6  -1   3  18  -1
+ *   1  12  20  15  19      1  12  20  15  19
  * 
  *  Bingo Card 1
- *   3  15  -1   2  -1
- *   9  18  -1  -1   5
- *  19   8   7  25  23
- *  20  -1  10  24   4
- *  14  21  16  12   6
+ *   3  15   0   2  22      3  15  -1  -1  22
+ *   9  18  13  17   5     -1  18  -1  -1  -1
+ *  19   8   7  25  23     19   8  -1  25  -1
+ *  20  11  10  24   4     20  -1  -1  -1  -1
+ *  14  21  16  12   6     -1  -1  -1  12   6
  * 
  *  Bingo Card 2
- *  14  21  -1  24   4
- *  10  16  15   9  19
- *  18   8  23  26  20
- *  -1  -1  -1   6   5
- *   2  -1  12   3   7
+ *  14  21  17  24   4     -1  -1  -1  -1  -1
+ *  10  16  15   9  19     10  16  15  -1  19
+ *  18   8  23  26  20     18   8  -1  26  20
+ *  22  11  13   6   5     22  -1  13   6  -1
+ *   2   0  12   3   7     -1  -1  12   3  -1
  * 
- * Result Part 1 = 0
- * Result Part 2 = 0
+ * 
+ * -------------------------------------------------------------------------------------
+ * 
+ * Bingo-Bingocard
+ *  Bingo Card 2
+ *  14  21  17  24   4     -1  -1  -1  -1  -1
+ *  10  16  15   9  19     10  16  15  -1  19
+ *  18   8  23  26  20     18   8  -1  26  20
+ *  22  11  13   6   5     22  -1  13   6  -1
+ *   2   0  12   3   7     -1  -1  12   3  -1
+ * 
+ * last nr 24
+ * non hit 188
+ * 
+ * -------------------------------------------------------------------------------------
+ * 
+ * Bingo Last Card
+ *  Bingo Card 1
+ *   3  15   0   2  22      3  15  -1  -1  22
+ *   9  18  13  17   5     -1  18  -1  -1  -1
+ *  19   8   7  25  23     19   8  -1  25  -1
+ *  20  11  10  24   4     20  -1  -1  -1  -1
+ *  14  21  16  12   6     -1  -1  -1  12   6
+ * 
+ * last nr 13
+ * non hit 148
+ * 
+ * Result Part 1 = 4512
+ * Result Part 2 = 1924
  * 
  * Day 04 - End
+ * 
+ * 
+ * -------------------------------------------------------------------------------------
+ * 
+ * Bingo-Bingocard
+ *  Bingo Card 66
+ *  84  78   3  44  96     84  78   3  44  96
+ *  59  86  70  80  48     59  86  70  80  -1
+ *  93  88  52  43  61     93  -1  52  -1  61
+ *  95  66  46  62  58     -1  -1  -1  -1  -1
+ *   5  25   6  85  99      5  25   6  85  99
+ * 
+ * last nr 66
+ * non hit 1026
+ * 
+ * -------------------------------------------------------------------------------------
+ * 
+ * Bingo Last Card
+ *  Bingo Card 31
+ *  96  59  72   6  38     -1  -1  -1  -1  -1
+ *  60  70  76  82  46     -1  70  -1  -1  -1
+ *  47  53  51  64  98     -1  53  51  -1  -1
+ *  44  25  69  81  33     -1  25  -1  -1  33
+ *  73  52  10  74  55     73  -1  -1  -1  -1
+ * 
+ * last nr 6
+ * non hit 305
+ * 
+ * Result Part 1 = 67716
+ * Result Part 2 = 1830
  */
 
 
@@ -130,12 +216,14 @@ class BingoCard
 
     last_number         : number = 0;
 
+    knz_has_bingo       : boolean = false;
+
     constructor( pBingoCardNr : number ) 
     {
         this.bingo_card_nr = pBingoCardNr;
 
         this.bingo_numbers_value = new Array<number>( BINGO_ROWS * BINGO_COLS );
-        this.bingo_numbers_hit   = new Array<number>( BINGO_ROWS * BINGO_COLS );
+        this.bingo_numbers_hit   = new Array<number>( 1 );
     }
 
     public reset()
@@ -143,24 +231,105 @@ class BingoCard
         this.bingo_numbers_hit = [ ...this.bingo_numbers_value ];
 
         this.last_number       = -1;
+
+        this.knz_has_bingo     = false;
     }
 
     public checkValue( pNumber : number ) : boolean
     {
-        if( this.bingo_numbers_hit.includes( pNumber ) )
+        if ( this.knz_has_bingo ) return false;
+
+        this.last_number = pNumber;
+
+        if( this.bingo_numbers_hit.includes( this.last_number ) )
         {
             for ( let bingo_idx : number = 0; bingo_idx < this.bingo_numbers_hit.length; bingo_idx++ )
             {
-                if ( this.bingo_numbers_hit[ bingo_idx ] === pNumber )
+                if ( this.bingo_numbers_hit[ bingo_idx ] === this.last_number )
                 {
                     this.bingo_numbers_hit[ bingo_idx ] = -1;
                 }
             }
 
-            return true;
+            if ( this.checkBingo() )
+            {
+                this.knz_has_bingo = true;
+
+                return true;
+            }
         }
 
         return false;
+    }
+
+    public checkBingo() : boolean 
+    {
+        for ( let row_nr : number = 0; row_nr < BINGO_ROWS; row_nr++ )
+        {
+            let row_index : number = row_nr * BINGO_COLS;
+
+            let row_has_bingo : boolean = true;
+
+            for ( let col_nr : number = 0; ( col_nr < BINGO_COLS ) && ( row_has_bingo ); col_nr++ )
+            {
+                if ( this.bingo_numbers_hit[ row_index + col_nr ]! >= 0 ) 
+                {
+                    row_has_bingo = false;
+
+                    break;
+                }
+            }
+
+            if ( row_has_bingo )
+            {
+                return true;
+            }
+        }
+
+        for ( let col_nr : number = 0; col_nr < BINGO_COLS; col_nr++ )
+        {
+            let col_has_bingo : boolean = true;
+
+            for ( let row_nr : number = 0; row_nr < BINGO_ROWS; row_nr++ )
+            {
+                if ( this.bingo_numbers_hit[ ( row_nr * BINGO_COLS ) + col_nr ]! >= 0 ) 
+                {
+                    col_has_bingo = false;
+
+                    break;
+                }
+            }
+
+            if ( col_has_bingo )
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public getSumNonHitFields() : number
+    {
+        let sum_non_hit_fields : number = 0;
+
+        for ( let bingo_idx : number = 0; bingo_idx < this.bingo_numbers_hit.length; bingo_idx++ )
+        {
+            if ( this.bingo_numbers_hit[ bingo_idx ]! >= 0 )
+            {
+                sum_non_hit_fields += this.bingo_numbers_hit[ bingo_idx ]!;
+            }
+        }
+
+        return sum_non_hit_fields;
+    }
+
+    public calcResultPart01() : number 
+    {
+        wl( "Last Number   " + this.last_number          );
+        wl( "Non Hit Value " + this.getSumNonHitFields() );
+
+        return this.getSumNonHitFields() * this.last_number;
     }
 
     public addBingoRow( pRow : number, pInput : string ) : void 
@@ -174,7 +343,7 @@ class BingoCard
             this.bingo_numbers_value[ bingo_idx + col_nr ] = num_array[ col_nr ]!;
         }
     }
-
+    
     public getStringValues() : string 
     {
         let str_result : string = "";
@@ -219,9 +388,13 @@ class BingoCard
 
     public toString() : string 
     {
-        let str_result : string = " Bingo Card " + this.bingo_card_nr + "\n" + this.getStringHit();
+        let card_hit : string = this.getStringHit();
 
-        return str_result;
+        let card_val : string = this.getStringValues();
+
+        let cards_combined : string = combineStrings( card_val, card_hit );
+
+        return " Bingo Card " + this.bingo_card_nr + "\n" + cards_combined;
     }
 }
 
@@ -236,7 +409,7 @@ function calcArray( pArray : string[], pKnzDebug : boolean = true ) : void
     let result_part_01 : number = 0;
     let result_part_02 : number = 0;
 
-    let numbers_to_call : string = "22,13,17,11,0,"; // +  pArray[0]!;
+    let numbers_to_call : string = pArray[0]!;
 
     let bingo_cards   : BingoCard[] = [];
 
@@ -268,7 +441,10 @@ function calcArray( pArray : string[], pKnzDebug : boolean = true ) : void
     {
         cur_bingo_card.reset();
 
-        wl( cur_bingo_card.toString() );
+        if ( pKnzDebug )
+        {
+            wl( cur_bingo_card.toString() );
+        }
     }
 
     /*
@@ -278,15 +454,42 @@ function calcArray( pArray : string[], pKnzDebug : boolean = true ) : void
      */
     wl( numbers_to_call );
 
+    let bingo_first_card : BingoCard | undefined = undefined;
+    let bingo_last_card  : BingoCard | undefined = undefined;
+
     const number_array : number[] = numbers_to_call.trim().split( "," ).map( Number ); 
 
     let knz_break_zuf : boolean = false;
 
     for ( let idx_num_array : number = 0; idx_num_array < numbers_to_call.length; idx_num_array++ )
     {
+        wl( "Nr. " + padL( idx_num_array, 3 ) + " = " + padL( number_array[ idx_num_array ] ?? -4 , 3 ))
+
         for ( let cur_bingo_card of bingo_cards )
         {
-            cur_bingo_card.checkValue( number_array[ idx_num_array ]! );
+            knz_break_zuf = cur_bingo_card.checkValue( number_array[ idx_num_array ]! );
+
+            if ( knz_break_zuf )
+            {
+                if ( bingo_first_card === undefined )
+                {
+                    bingo_first_card = cur_bingo_card;
+                }
+
+                bingo_last_card = cur_bingo_card;
+            }
+        }
+    }
+
+    if ( pKnzDebug )
+    {
+        wl( "" );
+        wl( "-------------------------------------------------------------------------------------" );
+        wl( "" );
+
+        for ( let cur_bingo_card of bingo_cards )
+        {
+            wl( cur_bingo_card.toString() );
         }
     }
 
@@ -294,20 +497,24 @@ function calcArray( pArray : string[], pKnzDebug : boolean = true ) : void
     wl( "-------------------------------------------------------------------------------------" );
     wl( "" );
 
-    for ( let cur_bingo_card of bingo_cards )
+    if ( bingo_first_card !== undefined )
     {
-        wl( cur_bingo_card.toString() );
+        wl( "Bingo First Card" );
+        wl( bingo_first_card.toString() );
+
+        result_part_01 = bingo_first_card.calcResultPart01();
     }
 
-    /*
-     * *******************************************************************************************************
-     * 
-     * *******************************************************************************************************
-     */
+    wl( "" );
+    wl( "-------------------------------------------------------------------------------------" );
+    wl( "" );
 
-
-    if ( pKnzDebug )
+    if ( bingo_last_card !== undefined )
     {
+        wl( "Bingo Last Card" );
+        wl( bingo_last_card.toString() );
+
+        result_part_02 = bingo_last_card.calcResultPart01();
     }
 
     wl( "" );
