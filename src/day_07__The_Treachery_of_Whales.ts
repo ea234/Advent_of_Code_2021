b3ab3ba36cc6b508743b0586248c1f0480ce2549
ea234 <anyang@gmx.de>
2026-04-18 08:01:50 +0200

    Day 07 - Part 2

--- src/day_07__The_Treachery_of_Whales.ts
+++ src/day_07__The_Treachery_of_Whales.ts
@@ -42,14 +42,44 @@ import * as readline from 'readline';
  * Nr.  15  cur_fuel 103   min fuel at pos   2  val   37
  * Nr.  16  cur_fuel 111   min fuel at pos   2  val   37
  * 
+ * Nr.   0  cur_fuel 290   min fuel at pos   0  val  290
+ * Nr.   1  cur_fuel 242   min fuel at pos   1  val  242
+ * Nr.   2  cur_fuel 206   min fuel at pos   2  val  206
+ * Nr.   3  cur_fuel 183   min fuel at pos   3  val  183
+ * Nr.   4  cur_fuel 170   min fuel at pos   4  val  170
+ * Nr.   5  cur_fuel 168   min fuel at pos   5  val  168
+ * Nr.   6  cur_fuel 176   min fuel at pos   5  val  168
+ * Nr.   7  cur_fuel 194   min fuel at pos   5  val  168
+ * Nr.   8  cur_fuel 223   min fuel at pos   5  val  168
+ * Nr.   9  cur_fuel 262   min fuel at pos   5  val  168
+ * Nr.  10  cur_fuel 311   min fuel at pos   5  val  168
+ * Nr.  11  cur_fuel 370   min fuel at pos   5  val  168
+ * Nr.  12  cur_fuel 439   min fuel at pos   5  val  168
+ * Nr.  13  cur_fuel 518   min fuel at pos   5  val  168
+ * Nr.  14  cur_fuel 607   min fuel at pos   5  val  168
+ * Nr.  15  cur_fuel 707   min fuel at pos   5  val  168
+ * Nr.  16  cur_fuel 817   min fuel at pos   5  val  168
+ * 
  * -------------------------------------------------------------------------------------
  * 
  * Result Part 1 = 37
- * Result Part 2 = 0
+ * Result Part 2 = 168
  * 
  * Day 07 - End
  * 
+ * 
+ * -------------------------------------------------------------------------------------
+ * 
+ * Result Part 1 = 336721
+ * Result Part 2 = 91638945
+ * 
+ * 
  */
+
+type PropertieMap = Record< number, number >;
+
+let fuel_map : PropertieMap = {};
+
 
 function wl( pString : string ) // wl = short for "writeLog"
 {
@@ -70,16 +100,23 @@ function padL( pInput : string | number, pPadLeft : number ) : string
 }
 
 
-function padR( pInput : string | number, pPadRight : number ) : string 
+function getFuel( pDistance : number ) : number 
 {
-    let str_result : string = pInput.toString();
+    let fuel_value : number = fuel_map[ pDistance ] ?? -1;
 
-    while ( str_result.length < pPadRight )
-    { 
-        str_result = str_result + " ";
+    if ( fuel_value === -1 ) 
+    {
+        fuel_value = 0;
+
+        for ( let step_nr = 1; step_nr <= pDistance; step_nr++ )
+        {
+            fuel_value += step_nr;
+        }
+
+        fuel_map[ pDistance ] = fuel_value;
     }
 
-    return str_result;
+    return fuel_value;
 }
 
 
@@ -89,12 +126,6 @@ function calcArray( pArray : string[], pKnzDebug : boolean = true ) : void
     let result_part_02 : number = 0;
 
     const number_array : number[] = pArray[0]!.trim().split( "," ).map( Number ); 
-
-    /*
-     * *******************************************************************************************************
-     * Callculating Part 1 
-     * *******************************************************************************************************
-     */
 
     let min_pos : number = Number.MAX_SAFE_INTEGER;
 
@@ -116,8 +147,8 @@ function calcArray( pArray : string[], pKnzDebug : boolean = true ) : void
      * *******************************************************************************************************
      */
 
-    let min_fuel_val : number = Number.MAX_SAFE_INTEGER;
-    let min_fuel_pos  : number = Number.MAX_SAFE_INTEGER;
+    let min_fuel_val_01 : number = Number.MAX_SAFE_INTEGER;
+    let min_fuel_pos_01 : number = Number.MAX_SAFE_INTEGER;
 
     for ( let cur_pos : number = min_pos; cur_pos <= max_pos; cur_pos++ )
     {
@@ -125,37 +156,58 @@ function calcArray( pArray : string[], pKnzDebug : boolean = true ) : void
 
         for ( let idx_num_array : number = 0; idx_num_array < number_array.length; idx_num_array++ )
         {
-            if ( cur_pos < number_array[ idx_num_array ]! )
-            {
-                cur_fuel += Math.abs( number_array[ idx_num_array ]! - cur_pos );
-            }
-            else
-            {
-                cur_fuel += Math.abs( cur_pos - number_array[ idx_num_array ]! );
-            }
+            cur_fuel += Math.abs( cur_pos - number_array[ idx_num_array ]! );
         }
 
-        if ( cur_fuel < min_fuel_val )
+        if ( cur_fuel < min_fuel_val_01 )
         {
-            min_fuel_val = Math.min( min_fuel_val, cur_fuel );
-            min_fuel_pos = cur_pos;
+            min_fuel_val_01 = Math.min( min_fuel_val_01, cur_fuel );
+            min_fuel_pos_01 = cur_pos;
         }
 
-        wl( "Nr. " + padL( cur_pos, 3 ) + "  cur_fuel " + padL( cur_fuel, 3 ) + "   min fuel at pos " + padL( min_fuel_pos, 3 ) + "  val  " + padL( min_fuel_val, 3 ) + " "  )
-
+        wl( "Nr. " + padL( cur_pos, 3 ) + "  cur_fuel " + padL( cur_fuel, 3 ) + "   min fuel at pos " + padL( min_fuel_pos_01, 3 ) + "  val  " + padL( min_fuel_val_01, 3 ) + " "  )
     }
-
-    result_part_01 = min_fuel_val;
 
     /*
      * *******************************************************************************************************
-     * Calculating the result-values for part 1 and 2
+     * Callculating Part 2 
      * *******************************************************************************************************
      */
+
+    wl( "" );
+
+    let min_fuel_val_02 : number = Number.MAX_SAFE_INTEGER;
+    let min_fuel_pos_02 : number = Number.MAX_SAFE_INTEGER;
+
+    for ( let cur_pos : number = min_pos; cur_pos <= max_pos; cur_pos++ )
+    {
+        let cur_distance : number = 0;
+
+        let cur_fuel : number = 0;
+
+        for ( let idx_num_array : number = 0; idx_num_array < number_array.length; idx_num_array++ )
+        {
+            cur_distance = Math.abs( cur_pos - number_array[ idx_num_array ]! );
+
+            cur_fuel += getFuel( cur_distance );
+        }
+
+        if ( cur_fuel < min_fuel_val_02 )
+        {
+            min_fuel_val_02 = Math.min( min_fuel_val_02, cur_fuel );
+            min_fuel_pos_02 = cur_pos;
+        }
+
+        wl( "Nr. " + padL( cur_pos, 3 ) + "  cur_fuel " + padL( cur_fuel, 3 ) + "   min fuel at pos " + padL( min_fuel_pos_02, 3 ) + "  val  " + padL( min_fuel_val_02, 3 ) + " "  )
+    }
+
     wl( "" );
     wl( "-------------------------------------------------------------------------------------" );
     wl( "" );
 
+    result_part_01 = min_fuel_val_01;
+
+    result_part_02 = min_fuel_val_02;
 
     wl( "" );
     wl( "Result Part 1 = " + result_part_01 );
