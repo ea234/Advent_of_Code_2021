a9a65513ffcadf1fc582a4420c026c1742224575
ea234 <anyang@gmx.de>
2026-04-23 11:43:21 +0200

    Day 08 - Part 2 - function to print segment display

--- src/day_08__Seven_Segment_Search.ts
+++ src/day_08__Seven_Segment_Search.ts
@@ -25,6 +25,20 @@ import * as readline from 'readline';
  * .    f  e    f  .    f  e    f  .    f
  *  gggg    gggg    ....    gggg    gggg
  * 
+ * not valid
+ * b c 
+ * bc ef
+ * bf
+ * ce
+ * ag
+ * ab
+ * ac
+ * af
+ * ag
+ * aefg
+ * adg
+ * 
+ * 
  * 0 = a b c   e f g = 6
  * 1 =     c     f   =   2 
  * 2 = a   c d e   g = 5
@@ -35,6 +49,27 @@ import * as readline from 'readline';
  * 7 = a   c     f   =   3
  * 8 = a b c d e f g =   7
  * 9 = a b c d   f g = 6
+ * 
+ * 
+ * 
+ * fdgacbe = 8 S = 8 = a b c d e f g = 
+ * 
+ * cefdb   = 5 S = 2 = a   c d e   g   
+ *                 3 = a   c d   f g
+ *                 5 = a b   d   f g
+ * 
+ * cefbgd  = 6 S = 6 = a b   d e f g
+ *                 9 = a b c d   f g
+ * 
+ * gcbe    = 4 S = 4 =   b c d   f   
+ * 
+ * 
+ * be  = 2 S = 1 = b e 
+ * edb = 3 S = 7  = d = uniqe 
+ * 
+ * 
+ * cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd 
+ * 
  * 
  * miss_wired_a = 7_1,           8_1
  * miss_wired_b = 4_1,           8_2 
@@ -159,7 +194,31 @@ class SevenSegmentDisplay
     // miss_wired_e  : string = "e";
     // miss_wired_f  : string = "f";
     // miss_wired_g  : string = "g";
+ /*
+ acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf
 
+ dddd
+e    a
+e    a
+ ffff
+g    b
+g    b
+ cccc
+
+So, the unique signal patterns would correspond to the following digits:
+
+    acedgfb: 8
+    cdfbe: 5
+    gcdfa: 2
+    fbcad: 3
+    dab: 7
+    cefabd: 9
+    cdfgeb: 6
+    eafb: 4
+    cagedb: 0
+    ab: 1
+
+ */
     public setMisswired( pString : string ) : boolean 
     {
         if ( pString.trim() !== "" )
@@ -255,6 +314,21 @@ class SevenSegmentDisplay
 
         return res_count;
     }
+
+    public getDebgString() : string 
+    {
+        let str_result : string = "";
+
+        str_result += "\n a = " + this.vek_miss_wired[ "a" ];
+        str_result += "\n b = " + this.vek_miss_wired[ "b" ];
+        str_result += "\n c = " + this.vek_miss_wired[ "c" ];
+        str_result += "\n d = " + this.vek_miss_wired[ "d" ];
+        str_result += "\n e = " + this.vek_miss_wired[ "e" ];
+        str_result += "\n f = " + this.vek_miss_wired[ "f" ];
+        str_result += "\n g = " + this.vek_miss_wired[ "g" ];
+
+        return str_result;
+    }
 }
 
 
@@ -291,6 +365,8 @@ function calcArray( pArray : string[], pKnzDebug : boolean = true ) : void
         seven_segment_display.setMisswired( part1! );
 
         wl( "Input " + part2! + " Number " + seven_segment_display.getNumber( part2! ) );
+
+        wl( seven_segment_display.getDebgString() );
     }
 
     wl( "" );
@@ -339,6 +415,16 @@ function getTestArray1() : string[]
 {
     const array_test: string[] = [];
 
+    array_test.push( "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf" );
+    
+    return array_test;
+}
+
+
+function getTestArray2() : string[] 
+{
+    const array_test: string[] = [];
+
     array_test.push( "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe" );
     array_test.push( "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc" );
     array_test.push( "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg" );
@@ -349,18 +435,61 @@ function getTestArray1() : string[]
     array_test.push( "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef" );
     array_test.push( "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb" );
     array_test.push( "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce" );
-
+    
     return array_test;
 }
 
+
+
+function printLetter( pInput : string ) : string 
+{
+
+/*
+ *   0:     
+ *  aaaa   
+ * b    c  
+ * b    c  
+ *  ....   
+ * e    f  
+ * e    f  
+ *  gggg   
+*/
+    let varrk : string[] = pInput.split("" );
+
+    let str_space_btwn : string = "    ";
+    let str_space_surround : string = " ";
+
+    let str_a : string = varrk.includes( "a" ) ? "a" : " ";
+    let str_b : string = varrk.includes( "b" ) ? "b" : " ";
+    let str_c : string = varrk.includes( "c" ) ? "c" : " ";
+    let str_d : string = varrk.includes( "d" ) ? "d" : " ";
+    let str_e : string = varrk.includes( "e" ) ? "e" : " ";
+    let str_f : string = varrk.includes( "f" ) ? "f" : " ";
+    let str_g : string = varrk.includes( "g" ) ? "g" : " ";
+
+    let str_return : string = "";
+
+    str_return += str_space_surround + " " + str_a.repeat( 4 ) + " " + str_space_surround + "\n"
+    str_return += str_space_surround + str_b + str_space_btwn + str_c + str_space_surround + "\n"
+    str_return += str_space_surround + str_b + str_space_btwn + str_c + str_space_surround + "\n"
+    str_return += str_space_surround + " " + str_d.repeat( 4 ) + " " + str_space_surround + "\n"
+    str_return += str_space_surround + str_e + str_space_btwn + str_f + str_space_surround + "\n"
+    str_return += str_space_surround + str_e + str_space_btwn + str_f + str_space_surround + "\n"
+    str_return += str_space_surround + " " + str_g.repeat( 4 ) + " " + str_space_surround + "\n"
+
+    console.log( varrk );
+
+    return str_return;
+}
 
 wl( "" );
 wl( "Day 8: Seven Segment Search" );
 wl( "" );
 
-calcArray( getTestArray1(), true );
+//calcArray( getTestArray1(), true );
 
 //checkReaddatei();
 
+wl( printLetter( "abcdefg" ) );
 wl( "" )
 wl( "Day 8 - End " );
